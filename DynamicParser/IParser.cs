25c99d4a7baf3a4927a793ea6ee6544157f74cf8
Jack Hsu <jack.h.mocha@gmail.com>
2023-07-19 13:51:18 -0400

    Add IParser interface

---
+++ DynamicParser/IParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicParser
+{
+    public interface IParser
+    {
+        dynamic Parse(string configuration);
+    }
+}
