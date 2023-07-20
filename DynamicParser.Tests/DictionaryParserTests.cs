3310074c23f490570ecdc18fa3939b4efe36220b
Jack Hsu <jack.h.mocha@gmail.com>
2023-07-19 21:21:16 -0400

    Add more unit tests for DictionaryParser.Parse()

--- DynamicParser.Tests/DictionaryParserTests.cs
+++ DynamicParser.Tests/DictionaryParserTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,16 +16,33 @@ namespace DynamicParser.Tests
             this._subject = new DictionaryParser();
         }
 
-        [Fact]
-        public void Parse_WhenCalled_ShouldParseInput()
+        public class TestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 
+                    @"FirstName: John;  
+
+                    LastName: Doe;
+                    IsValid: true;
+                    Age: 100" };
+                yield return new object[] {
+                    @"FirstName: John     ;  
+
+                        LastName  :   Doe;
+                        IsValid   :   true;
+                    Age: 100" };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+
+        [Theory]
+        [ClassData(typeof(TestData))]
+        public void Parse_WhenCalled_ShouldParseInput(string config)
         {
             //arange
-            var config = @"FirstName: John;  
-
-                LastName: Doe;
-                IsValid: true;
-                Age: 100";
-
             var shouldParsed = new Configuration(new Dictionary<string, dynamic>()
             {
                 {"FirstName", "John"},
@@ -43,6 +61,63 @@ namespace DynamicParser.Tests
             Assert.Equal(shouldParsed["LastName"], actualParsed["LastName"]);
             Assert.Equal(shouldParsed["IsValid"], actualParsed["IsValid"]);
             Assert.Equal(shouldParsed["Age"], actualParsed["Age"]);
+        }
+
+        [Fact]
+        public void Parse_WhenThereIsEmptyKey_ShouldThrowEmptyKeyException()
+        {
+            //arange
+            var config = @"FirstName: John;  
+
+                      : Doe;
+                    IsValid: true;
+                    Age: 100";
+            
+            //assert
+            Assert.Throws<EmptyKeyException>(() => _subject.Parse(config));
+        }
+
+
+        [Fact]
+        public void Parse_WhenThereIsDuplicateKey_ShouldThrowDuplicateKeyException()
+        {
+            //arange
+            var config = @"FirstName: John;  
+
+                     FirstName : Doe;
+                    IsValid: true;
+                    Age: 100";
+
+            //assert
+            Assert.Throws<DuplicateKeyException>(() => _subject.Parse(config));
+        }
+
+        [Fact]
+        public void Parse_KeyDoesNotStartWhichLetter_ShouldThrowInvalidKeyException()
+        {
+            //arange
+            var config = @"FirstName: John;  
+
+                    2LastName : Doe;
+                    IsValid: true;
+                    Age: 100";
+
+            //assert
+            Assert.Throws<InvalidKeyException>(() => _subject.Parse(config));
+        }
+
+        [Fact]
+        public void Parse_KeyContainsNonLetterOrDigit_ShouldThrowInvalidKeyException()
+        {
+            //arange
+            var config = @"FirstName: John;  
+
+                    Last$Name : Doe;
+                    IsValid: true;
+                    Age: 100";
+
+            //assert
+            Assert.Throws<InvalidKeyException>(() => _subject.Parse(config));
         }
     }
 }
